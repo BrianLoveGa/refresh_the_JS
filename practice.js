9ca06585bccf1b1c85b52d65146478529e8128fb
Brian Loveless <Brian.loveless1@yahoo.com>
2021-04-21 13:34:52 -0400

    lunch break

--- practice.js
+++ practice.js
@@ -46,30 +46,51 @@ var zeroByDiv = 0 / num3;
 console.log(zeroByDiv);
 console.log(typeof zeroByDiv);
 
-
 let numString = 5 + " five";
 
-console.log(typeof(numString));
+console.log(typeof numString);
 console.log(numString);
 ////////////
 
 // toString()    ->  int to string
 // parseInt()   -> string to int (rounded)
 // parseFloat() -> string to float (not as rounded)
-// toFixed()   ->  rounds float to given position    
-     //   toFixed(#)  ... # = places past decimal to round to - 0 by default.
-
-
-
+// toFixed()   ->  rounds float to given position
+//   toFixed(#)  ... # = places past decimal to round to - 0 by default.
 
 ///  stringy strings
 
-var dqs1 = "This is a \"javascript\" string";
+var dqs1 = 'This is a "javascript" string';
 var dqs2 = 'This is a "javascript" string';
-var sqs1 = 'This is a \'javascript\' string';
+var sqs1 = "This is a 'javascript' string";
 var sqs2 = "This is a 'javascript' string";
+var simp = "Here, is a simple string";
 
 console.log(dqs1);
 console.log(dqs2);
 console.log(sqs1);
 console.log(sqs2);
+console.log(simp);
+
+console.log(simp.slice(0, 7));
+console.log(simp.slice(-6));
+console.log(simp.slice(4, 17));   // start index , stop index inclusive
+console.log(simp.length);
+console.log(simp.indexOf('simple'));
+console.log(simp.indexOf("This"));
+console.log(simp.lastIndexOf("s"));
+console.log(simp.lastIndexOf("g"));
+
+console.log(simp.substr(5, 12));  // start index , number of spaces to go
+
+/// .toUppercase() .toLowerCase() .trim() .concat()
+
+/// .charAt(#)   = whats at index # ?
+
+/// .split()   -> string to array
+
+
+console.log(simp.split(' '));
+console.log(simp.split(','));
+console.log(simp.split(""));
+console.log(simp.split());
